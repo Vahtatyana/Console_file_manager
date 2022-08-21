6386d0c3f064e4d0886aac16fae654c8695baccb
Vahtatyana <Vahtatyana1987@gmail.com>
2022-08-21 20:49:01 +0300

    1v

--- file_manager.py
+++ file_manager.py
@@ -4,71 +4,85 @@ import Vactory
 import Bank
 
 
-def list_dir(a = 1):
-    l_dir = os.listdir()
-    for i in l_dir:
-        if a == 1:
-            print(i)
-        elif os.path.isdir(i) and a == 2:
-            print(i)
-        elif os.path.isfile(i) and a == 3:
-            print(i)
+print('1. создать папку')
+print('2. удалить (файл/папку)')
+print('3. копировать (файл/папку)')
+print('4. просмотр содержимого рабочей директории')
+print('5. посмотреть только папкии')
+print('6. посмотреть только файлы')
+print('7. просмотр информации об операционной систем')
+print('8. создатель программы')
+print('9. играть в викторину')
+print('10. мой банковский счет')
+print('11. смена рабочей директории')
+print('12. выход')
 
-while True:
-    print('1. создать папку')
-    print('2. удалить (файл/папку)')
-    print('3. копировать (файл/папку)')
-    print('4. просмотр содержимого рабочей директории')
-    print('5. посмотреть только папкии')
-    print('6. посмотреть только файлы')
-    print('7. просмотр информации об операционной систем')
-    print('8. создатель программы')
-    print('9. играть в викторину')
-    print('10. мой банковский счет')
-    print('11. смена рабочей директории')
-    print('12. выход')
 
-    choice = input('Выберите пункт меню')
-    if choice == '1':
-        name_dir = input('Введите имя папки:')
-        if not os.path.exists(name_dir):
-            os.mkdir(name_dir)
-            print(f'Папка {name_dir} создана.')
-        else:
-            print(f'Папка {name_dir} уже существует.')
-    elif choice == '2':
-        name_dir = input('Введите имя папки или файла: ')
-        if  os.path.exists(name_dir) and os.path.isdir(name_dir):
-            os.rmdir(name_dir)
-            print(f'Папка {name_dir} удалена.')
-        elif  os.path.exists(name_dir) and os.path.isfile(name_dir):
-            os.remove(name_dir)
-            print(f'Файл {name_dir} удален.')
-        else:
-            print(f'Папки или файла {name_dir} не существует.')
-    elif choice == '3':
-        name_dir = input('Введите имя папки или файла: ')
-        new_name_dir = input('Введите новое имя папки или файла: ')
-        shutil.copytree(name_dir, new_name_dir)
-        print(f'Папка {name_dir} скопирована. ')
-    elif choice == '4':
-        list_dir()
-    elif choice == '5':
-        list_dir(2)
-    elif choice == '6':
-        list_dir(3)
-    elif choice == '7':
-        print(os.name)
-    elif choice == '8':
-        print('Создатель программы: Вахрушева ТС')
-    elif choice == '9':
-        Vactory.game()
-    elif choice == '10':
-        Bank.bill()
-    elif choice == '11':
-        path = input('Введите новый путь:')
-        os.chdir(path)
-    elif choice == '12':
-        break
+choice = input('Выберите пункт меню: ')
+
+if choice == '1':
+    name_dir = input('Введите имя папки:')
+    if not os.path.exists(name_dir):
+        os.mkdir(name_dir)
+        print(f'Папка {name_dir} создана.')
     else:
-        print('Неверный пункт меню')
+        print(f'Папка {name_dir} уже существует.')
+
+elif choice == '2':
+    name_dir = input('Введите имя папки или файла: ')
+    if os.path.isdir(name_dir):
+        try:
+            os.rmdir(name_dir)
+            print('Папка удалена!')
+        except OSError:
+            print('Папка не пуста!')
+    elif os.path.isfile(name_dir):
+        os.remove(name_dir)
+        print('Файл удален!')
+    else:
+        print('Файл/папка не найден!')
+
+elif choice == '3':
+    name_dir = input('Введите имя папки или файла: ')
+    new_name_dir = input('Введите новое имя папки или файла: ')
+    shutil.copytree(name_dir, new_name_dir)
+    print(f'Папка {name_dir} скопирована. ')
+
+elif choice == '4':
+    if len(os.listdir()) == 0:
+        print('Директория пуста!')
+    else:
+        print(os.listdir())
+
+elif choice == '5':
+    for i in os.listdir():
+        if os.path.isdir(i):
+            print(i)
+
+elif choice == '6':
+    for i in os.listdir():
+        if os.path.isfile(i):
+            print(i)
+
+elif choice == '7':
+    print(os.name)
+
+elif choice == '8':
+    print('Создатель программы: Вахрушева ТС')
+
+elif choice == '9':
+    Vactory.game()
+
+elif choice == '10':
+    Bank.bill()
+
+elif choice == '11':
+    path = input('Введите новый путь:')
+    os.chdir(path)
+
+elif choice == '12':
+    print(f'Текущая рабочая директория: {os.getcwd()}')
+    directory = input('Введите название директории или путь:  ')
+    os.chdir(directory)
+else:
+    print ("Неверный пункт меню")
